f58eb73031a3b5d2bc38abbc2f19092e81efb294
sevbesau <seppe@sevbesau.xyz>
2024-02-19 21:04:40 +0100

    css improvements

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
+import './main.css';
+
 import { RouterProvider } from 'react-router-dom';
 import router from './router.tsx';
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,14 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{ts,tsx,js,jsx,html}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
