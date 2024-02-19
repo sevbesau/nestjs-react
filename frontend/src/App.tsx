f0331889dd1cd08bebcfecb4a6c6741bf9c3c479
sevbesau <seppe@sevbesau.xyz>
2024-02-19 20:47:40 +0100

    installed tailwindcss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,15 @@
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-import './App.css';
 
 function App() {
   return (
     <>
-      <div>
+      <div className="bg-gray-800 h-full flex justify-center items-center space-x-16">
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img className="w-64" src={viteLogo} alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="w-64 animate-spin" alt="React logo" />
         </a>
       </div>
     </>
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{ts,tsx,js,jsx,html}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
